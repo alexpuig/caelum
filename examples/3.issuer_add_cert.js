e06dd9697a0bcf6898c63c2085e5a90533c351be
Alex Puig <alex@caelumlabs.com>
2021-05-17 06:21:21 +0200

    Improve examples

--- examples/3.issuer_add_cert.js
+++ examples/3.issuer_add_cert.js
@@ -1,7 +1,7 @@
 const Caelum = require('../src/index')
 const GOVERNANCE = 'wss://substrate.tabit.caelumapp.com'
 const STORAGE = 'https://api.bigchaindb.caelumapp.com/api/v1/'
-const orgJson = require('./orgs/5FcSnjrPBAHkzqgkEz4WtnRGwLHkhHYRjYBKsDHPcozNce7R.json')
+const orgJson = require('./orgs/5GerGGPciAGfgmHj6zW4fDR7d4FedyX4sTCgNmbSrcenhcbg.json')
 const chalk = require('chalk')
 const log = console.log
 
